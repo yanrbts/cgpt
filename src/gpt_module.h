a91b64514a103fbce10ceee67692d818c53abe69
yanrbts <39860189+yanrbts@users.noreply.github.com>
2023-05-26 10:06:53 +0800

    Update gpt_module.h
    edit file

--- src/gpt_module.h
+++ src/gpt_module.h
@@ -1,1 +1,14 @@
+/*
+ * Processes different modules of the chatgpt module in modular
+ * mode and provides unified module interfaces.
+ */
+#ifndef __GPT_MODULE__
+#define __GPT_MODULE__
 
+struct gpt_module_s {
+    char *name;
+    char *url;
+    char *description;
+};
+
+#endif
